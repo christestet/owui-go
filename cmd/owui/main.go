306b8b97b69434827427a2467eb199d0f95d7f3b
christestet <christestautomation@gmail.com>
2026-02-28 08:31:33 +0100

    docs(cli docs): added docs generation within cli and using gh pages

--- cmd/owui/main.go
+++ cmd/owui/main.go
@@ -1,24 +1,11 @@
 package main
 
 import (
-	"github.com/christestet/owui-go/internal/cli/completion"
-	"github.com/christestet/owui-go/internal/cli/groups"
-	"github.com/christestet/owui-go/internal/cli/instances"
+	"github.com/christestet/owui-go/internal/cli"
 	"github.com/christestet/owui-go/internal/cli/root"
-	cliUpdate "github.com/christestet/owui-go/internal/cli/update"
-	"github.com/christestet/owui-go/internal/cli/users"
-	cliVersion "github.com/christestet/owui-go/internal/cli/version"
 )
 
 func main() {
-	// Register subcommands
-	root.Cmd.AddCommand(cliVersion.Cmd)
-	root.Cmd.AddCommand(cliUpdate.Cmd)
-	root.Cmd.AddCommand(completion.Cmd)
-	instances.Register(root.Cmd)
-	users.Register(root.Cmd)
-	groups.Register(root.Cmd)
-
-	// Execute the root command
+	cli.RegisterAll(root.Cmd)
 	root.Execute()
 }
